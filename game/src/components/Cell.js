ccb63d4bad832239932d52425d22a58c1abc67d5
Jesus Esquer <jme2791@gmail.com>
2020-06-16 21:41:54 -0700

    worked with styled components

--- game/src/components/Cell.js
+++ game/src/components/Cell.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from "react";
+import { StyledCell } from "./styles/StyledCell";
+import { TETROMINOS } from "../tetrominos";
 
-const Cell = ({type})=>(
-    <div>cell</div>
-)
+const Cell = ({ type }) => (
+  <StyledCell color={TETROMINOS[type].color} type={type}>
+    cell
+  </StyledCell>
+);
 
-export default Cell
+export default Cell;
--- game/src/components/Tetris.js
+++ game/src/components/Tetris.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createStage } from "../gameHelpers";
+import { StyledTetrisWrapper, StyledTetris } from "./styles";
 //Components
 import Stage from "./Stage";
 import Display from "./Display";
@@ -8,15 +9,19 @@ import StartButton from "./StartButton";
 const Tetris = () => {
   return (
     <div>
-      <Stage stage={createStage()}></Stage>
-      <aside>
-        <div>
-          <Display text="Score"></Display>
-          <Display text="Rows"></Display>
-          <Display text="Level"></Display>
-        </div>
-        <StartButton></StartButton>
-      </aside>
+      <StyledTetrisWrapper>
+        <StyledTetris>
+          <Stage stage={createStage()}></Stage>
+          <aside>
+            <div>
+              <Display text="Score"></Display>
+              <Display text="Rows"></Display>
+              <Display text="Level"></Display>
+            </div>
+            <StartButton></StartButton>
+          </aside>
+        </StyledTetris>
+      </StyledTetrisWrapper>
     </div>
   );
 };
