c7d3db5de68a0c53f88e7609ee09ad2feff07513
Jesus Esquer <jme2791@gmail.com>
2020-06-30 19:31:37 -0700

    Clear row when a line is completed

--- game/src/hooks/useStage.js
+++ game/src/hooks/useStage.js
@@ -3,9 +3,23 @@ import { createStage } from "../gameHelpers";
 
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
+  const [cleared, setCleared] = useState(0);
   console.log(player);
 
   useEffect(() => {
+    setCleared(0);
+
+    const sweepRows = (newStage) =>
+      newStage.reduce((acc, row) => {
+        if (row.findIndex((cell) => cell[0] === 0) === -1) {
+          setCleared((prev) => prev + 1);
+          acc.unshift(new Array(newStage[0].length).fill([0, "clear"]));
+          return acc;
+        }
+        acc.push(row);
+        return acc;
+      }, []);
+
     const updateStage = (prevStage) => {
       //clear Stage
       const newStage = prevStage.map((row) =>
@@ -25,6 +39,7 @@ export const useStage = (player, resetPlayer) => {
       });
       if (player.collided) {
         resetPlayer();
+        return sweepRows(newStage);
       }
 
       return newStage;
@@ -32,5 +47,5 @@ export const useStage = (player, resetPlayer) => {
     setStage((prev) => updateStage(prev));
   }, [player, resetPlayer]);
 
-  return [stage, setStage];
+  return [stage, setStage, cleared];
 };
